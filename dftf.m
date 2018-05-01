0ab4a38e88ba618800c4be5189eece278677b1ec
Saketh Kulapaka <sakethk98@gmail.com>
2018-05-01 19:21:07 +0530

    dft function ; commented

--- dftf.m
+++ dftf.m
@@ -1,9 +1,10 @@
 function [y] = dftf(x)
-%UNTITLED7 Summary of this function goes here
-%   Detailed explanation goes here
+%add an extra parameter (l) if you want to take in the length of the sequence
+%also and comment the next line
 l= length(x);
 temp = (-2)*i*pi/l;
 n= 0:l-1;
+
 for k = 0:l-1
     w = exp(n.*(temp*k));
     y(k+1)=x*w';
