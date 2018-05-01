f53f20d1f556fba42040412d4e90711d215e052e
Saketh Kulapaka <sakethk98@gmail.com>
2018-05-01 15:36:04 +0530

    decimation added

---
+++ decimation.m
@@ -0,0 +1,18 @@
+clc; clear all; close all;
+N = 50;
+n = 0:1:N-1;
+x= cos(2*pi*n/20);
+subplot(2,1,1);
+stem(n,x);
+hold on;
+a=1;
+b=fir1(5,0.5,'low');
+y=filter(b,a,x);
+stem(n,y);
+hold off;
+
+M=2;
+x=y(1:M:N);
+n1=1:1:(N/M);
+subplot(2,1,2);
+stem(n1,x);
